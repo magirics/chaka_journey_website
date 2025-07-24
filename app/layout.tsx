333ef38d99348bbfd809f715f9b095f2c4a481ca
Diego Figueroa Juarez <diego.undefined@gmail.com>
2025-07-24 11:46:37 -0500

    Set body as viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-screen max-w-screen">
         {children}
       </body>
     </html>
