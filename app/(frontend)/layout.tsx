73eb8a989f2a8da46874bd5bf2116ebf43a21899
Diego Figueroa Juarez <diego.undefined@gmail.com>
2025-08-06 08:07:28 -0500

    Move files from /app to /app/(frontend)

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import './styles.css'
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
-}
+export const metadata: Metadata = {
+  title: "",
+  description: "",
+};
 
-export default async function RootLayout(props: { children: React.ReactNode }) {
-  const { children } = props
-
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body>
-        <main>{children}</main>
+      <body className="min-h-screen w-screen">
+        <Navbar />
+        <main className="m-auto flex max-w-250 grow flex-col items-center">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
